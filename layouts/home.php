be8f78406294489b7271cc73dfbfb7b1485339a1
DengDeng317 <146617491+DengDeng317@users.noreply.github.com>
2024-10-08 22:08:56 +0800

    充滿問題的模態框

--- layouts/home.php
+++ layouts/home.php
@@ -1,79 +1,126 @@
 <div class="folder" id="current-folder">
-                    <div class="folder-header" onclick="toggleFolder('current')">
-                        <span class="arrow" id="arrow-current">▼</span>
-                        <img src="icon_current.png" alt="圖示" class="icon">
-                        <h2>當前紀錄</h2>
-                    </div>
+    <div class="folder-header" onclick="toggleFolder('current')">
+        <span class="arrow" id="arrow-current">▼</span>
+        <img src="icon_current.png" alt="圖示" class="icon">
+        <h2>當前紀錄</h2>
+    </div>
 
-                    <div id="current" class="content">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="table-responsive">
-                                    <div class="d-flex flex-row mb-3">
-                                        <a href="Addandedit.html" class="record mr-3">
-                                            <img src="image1.jpg" alt="圖片">
-                                            <p>紀錄名稱1</p>
-                                            <p>有效日期: 2024-12-31</p>
-                                        </a>
-                                        <a href="Addandedit.html" class="record mr-3">
-                                            <img src="image1.jpg" alt="圖片">
-                                            <p>紀錄名稱1</p>
-                                            <p>有效日期: 2024-12-31</p>
-                                        </a>
-                                        <a href="Addandedit.html" class="record">
-                                            <img src="image1.jpg" alt="圖片">
-                                            <p>紀錄名稱1</p>
-                                            <p>有效日期: 2024-12-31</p>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- 其他記錄事項 -->
-                    </div>
-                </div>
-
-                <div class="folder" id="waste-folder">
-                    <div class="folder-header" onclick="toggleFolder('waste')">
-                        <span class="arrow" id="arrow-waste">▼</span>
-                        <img src="icon_waste.png" alt="圖示" class="icon">
-                        <h2>廚餘桶紀錄</h2>
-                    </div>
-                    <div id="waste" class="content">
-                        <a href="Addandedit.html" class="record">
-                            <img src="image6.jpg" alt="圖片">
-                            <p>紀錄名稱6</p>
-                            <p>有效日期: 2024-11-30</p>
+    <div id="current" class="content">
+        <div class="row">
+            <div class="col-12">
+                <div class="table-responsive">
+                    <div class="d-flex flex-row mb-3">
+                        <a href="Addandedit.html" class="record mr-3">
+                            <img src="image1.jpg" alt="圖片">
+                            <p>紀錄名稱1</p>
+                            <p>有效日期: 2024-12-31</p>
                         </a>
-                        <!-- 其他記錄事項 -->
-                    </div>
-                </div>
-
-                <div class="folder" id="finished-folder">
-                    <div class="folder-header" onclick="toggleFolder('finished')">
-                        <span class="arrow" id="arrow-finished">▼</span>
-                        <img src="icon_finished.png" alt="圖示" class="icon">
-                        <h2>吃完紀錄</h2>
-                    </div>
-                    <div id="finished" class="content">
-                        <a href="Addandedit.html" class="record">
-                            <img src="image11.jpg" alt="圖片">
-                            <p>紀錄名稱11</p>
-                            <p>有效日期: 2024-10-31</p>
+                        <a href="Addandedit.html" class="record mr-3">
+                            <img src="image1.jpg" alt="圖片">
+                            <p>紀錄名稱1</p>
+                            <p>有效日期: 2024-12-31</p>
                         </a>
-                        <!-- 其他記錄事項 -->
+                        <a href="Addandedit.html" class="record">
+                            <img src="image1.jpg" alt="圖片">
+                            <p>紀錄名稱1</p>
+                            <p>有效日期: 2024-12-31</p>
+                        </a>
                     </div>
                 </div>
-                <div class="position-relative">
-                <div class="position-absolute bottom-0 end-0">
-                <a href="#" class="btn btn-primary btn-lg rounded-circle m-4" id="floatingBtn">
-                    +
-                    
-                    </a>
-                </div>
-                  
-                    </div>
-                <div class="container-fluid">
+            </div>
+        </div>
+        <!-- 其他記錄事項 -->
+    </div>
+</div>
 
+<div class="folder" id="waste-folder">
+    <div class="folder-header" onclick="toggleFolder('waste')">
+        <span class="arrow" id="arrow-waste">▼</span>
+        <img src="icon_waste.png" alt="圖示" class="icon">
+        <h2>廚餘桶紀錄</h2>
+    </div>
+    <div id="waste" class="content">
+        <a href="Addandedit.html" class="record">
+            <img src="image6.jpg" alt="圖片">
+            <p>紀錄名稱6</p>
+            <p>有效日期: 2024-11-30</p>
+        </a>
+        <!-- 其他記錄事項 -->
+    </div>
+</div>
+
+<div class="folder" id="finished-folder">
+    <div class="folder-header" onclick="toggleFolder('finished')">
+        <span class="arrow" id="arrow-finished">▼</span>
+        <img src="icon_finished.png" alt="圖示" class="icon">
+        <h2>吃完紀錄</h2>
+    </div>
+    <div id="finished" class="content">
+        <a href="Addandedit.html" class="record">
+            <img src="image11.jpg" alt="圖片">
+            <p>紀錄名稱11</p>
+            <p>有效日期: 2024-10-31</p>
+        </a>
+        <!-- 其他記錄事項 -->
+    </div>
+</div>
+<div class="folder" id="finished-folder">
+    <div class="folder-header" onclick="toggleFolder('finished')">
+        <span class="arrow" id="arrow-finished">▼</span>
+        <img src="icon_finished.png" alt="圖示" class="icon">
+        <h2>吃完紀錄</h2>
+    </div>
+    <div id="finished" class="content">
+        <a href="Addandedit.html" class="record">
+            <img src="image11.jpg" alt="圖片">
+            <p>紀錄名稱11</p>
+            <p>有效日期: 2024-10-31</p>
+        </a>
+        <!-- 其他記錄事項 -->
+    </div>
+</div>
+<div class="folder" id="finished-folder">
+    <div class="folder-header" onclick="toggleFolder('finished')">
+        <span class="arrow" id="arrow-finished">▼</span>
+        <img src="icon_finished.png" alt="圖示" class="icon">
+        <h2>吃完紀錄</h2>
+    </div>
+    <div id="finished" class="content">
+        <a href="Addandedit.html" class="record">
+            <img src="image11.jpg" alt="圖片">
+            <p>紀錄名稱11</p>
+            <p>有效日期: 2024-10-31</p>
+        </a>
+        <!-- 其他記錄事項 -->
+    </div>
+</div>
+<div class="fixed-bottom">
+    <div class="position-fixed" style="right: -10px; bottom: 50px;">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary btn-lg rounded-circle m-4" id="floatingBtn" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            +
+        </button>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+
+</div>
